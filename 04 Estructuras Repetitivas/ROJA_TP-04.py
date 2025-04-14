af2c494ff5042e901aa15b883d3757993729365e
Julio <julioroja987@gmail.com>
2025-04-14 16:23:22 -0300

    Se resolvieron los ejercicios restantes

--- 04 Estructuras Repetitivas/ROJA_TP-04.py
+++ 04 Estructuras Repetitivas/ROJA_TP-04.py
@@ -4,6 +4,7 @@
 # Comision: 20
 
 import random
+import math
 
 # EJERCICIO 1
 for x in range(1, 101):
@@ -48,12 +49,52 @@ while num != numero_secreto:
 print(f"¡Felicidades! Adivinaste el número secreto en {intentos} intentos.")
 
 # EJERCICIO 6
-
+for i in range(100, 0, -1):
+    print(i)
 
 # EJERCICIO 7
+num = int(input("Ingrese un número: "))
+if num > 0:
+    for i in range(0, num + 1):
+        print(i)
+else:
+    print("El numero debe ser positivo")
 
 # EJERCICIO 8
+ciclos = 100
+par = 0
+impar = 0
+positivo = 0
+negativo = 0
+for i in range(ciclos):
+    numeros = int(input("Ingrese un número: "))
+    if numeros % 2 == 0:
+        par += 1
+    else:
+        impar += 1
+
+    if numeros > 0:
+        positivo += 1
+    elif numeros < 0:
+        negativo += 1
+print(f"Cantidad de números pares: {par}")
+print(f"Cantidad de números impares: {impar}")
+print(f"Cantidad de números positivos: {positivo}")
+print(f"Cantidad de números negativos: {negativo}")
 
 # EJERCICIO 9
+ciclos = 100
+resultado = 0
+for i in range(ciclos):
+    num = int(input("Ingrese un número: "))
+    resultado += num
+print(f"La media de los números ingresados es: {resultado / ciclos}")
 
 # EJERCICIO 10
+num = int(input("Ingrese un número: "))
+invertido = 0
+while num > 0:
+    digito = num % 10
+    invertido = invertido * 10 + digito
+    num = num // 10
+print(f"El número invertido es: {invertido}")

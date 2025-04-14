96e6e666ecbbd03971269f1f175085ed373236bb
Julio <julioroja987@gmail.com>
2025-04-14 00:13:15 -0300

    Ejercicio 1 resuelto

---
+++ 04 Estructuras Repetitivas/ROJA_TP-04.py
@@ -0,0 +1,26 @@
+# Materia: Programacion I
+# Entrega de la Unidad 4 - Estructuras Repetitivas
+# Alumno: Julio Roja
+# Comision: 20
+
+# EJERCICIO 1
+for x in range(1, 101):
+    print(x)
+
+# EJERCICIO 2
+
+# EJERCICIO 3
+
+# EJERCICIO 4
+
+# EJERCICIO 5
+
+# EJERCICIO 6
+
+# EJERCICIO 7
+
+# EJERCICIO 8
+
+# EJERCICIO 9
+
+# EJERCICIO 10
